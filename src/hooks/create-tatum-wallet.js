251e3cf255967cbb77ca2a10f6d9448dfa36764f
emmytheo <chidi.mgbara@gmail.com>
2023-11-05 18:23:17 +0100

    first commit

--- src/hooks/create-tatum-wallet.js
+++ src/hooks/create-tatum-wallet.js
@@ -4,13 +4,18 @@ const tatumApi = new TatumApi(apiKey)
 
 export const createTatumWallet = async (context) => {
   console.log(`Running hook createTatumWallet on ${context.path}.${context.method}`)
-  let user = context.params.user || await context.app.service('users').find({ query: {email: context.data?.email} })
+  let user =
+    context.params.user ||
+    (await context.app.service('users').find({ query: { email: context.data?.email } }))
   user = context.params.user ? user : user.data[0]
-  const wallet = user && user._id ? await context.app.service('wallet').find({ query: {user_id: user._id} }) : null
+  const wallet =
+    user && user._id ? await context.app.service('wallet').find({ query: { user_id: user._id } }) : null
   const chain =
     wallet.data[0] && wallet.data[0].virtAcc
       ? null
-      : await context.app.service('blockchains').find({ query: {blockchain: user && user.blockchain ? user.blockchain : 'ethereum' }})
+      : await context.app
+          .service('blockchains')
+          .find({ query: { blockchain: user && user.blockchain ? user.blockchain : 'ethereum' } })
   const newVirtAcc =
     chain && chain.data[0] && chain.data[0].xpub
       ? await tatumApi.genVirtAcc({
@@ -18,12 +23,17 @@ export const createTatumWallet = async (context) => {
           xpub: chain.data[0].xpub
         })
       : null
-  const newDepAddr = await tatumApi.genVirtAccDepositAddress(newVirtAcc.id)
-  const final = await context.app.service('wallet').create({
-    user_id: user._id,
-    virtAcc: newVirtAcc,
-    depAddrs: newDepAddr
-  })
-  await context.app.service('users').patch(user._id, {wallet_id: final._id})
+  const newDepAddr =
+    newVirtAcc && newVirtAcc.id ? await tatumApi.genVirtAccDepositAddress(newVirtAcc.id) : null
+  const final =
+    newDepAddr && newDepAddr.address
+      ? await context.app.service('wallet').create({
+          user_id: user._id,
+          virtAcc: newVirtAcc,
+          depAddrs: newDepAddr
+        })
+      : null
+  const fnx =
+    final && final._id ? await context.app.service('users').patch(user._id, { wallet_id: final._id }) : null
   // console.log(final)
 }
